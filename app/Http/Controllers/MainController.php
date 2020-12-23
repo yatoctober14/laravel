fc845bc5886b76eabad9512a5fc1a7b2fe5f8848
AbdulnaserMohsen <naserahmed1995@gmail.com>
2020-12-23 21:57:54 +0200

    first commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    
+    public function about()
+    {
+    	return  view('about-us');
+    }
+
+}
