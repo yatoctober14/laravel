74c0ad9f7dfc638d352b077df435eeac5393c0cd
AbdulnaserMohsen <naserahmed1995@gmail.com>
2020-12-30 23:37:23 +0200

    login commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,5 +36,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        //return abort(404);
+        return view('auth.singin');
     }
 }
